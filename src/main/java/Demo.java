51ae187b52046cc367131697c8ef4eb190d68c2a
Muhammad Hassan Quddusi <hasan.quddusi@hotmail.com>
2021-09-30 04:30:40 -0500

    demo selenium script

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,4 +1,35 @@
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 
+	// A template for a simple selenium test case
+
+
+	public static void main(String[] args) {
+		// write your selenium code here 
+
+		//create driver object (chrome browser current) change to firefox(gecko) or IE as applicable
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("WebDriver.chrome.driver", "~/Downloads/webdriver/chromedriver.exe");
+		driver.get("https://google.com");
+
+		System.out.println(driver.getTitle()); 
+		//			Validate if you are on correct URL
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getWindowHandle());
+		driver.get("http://facebook.com");
+		System.out.println(driver.getTitle());
+		driver.navigate().back();
+		System.out.println(driver.getTitle());
+		driver.navigate().forward();
+
+		driver.get("https://www.dropbox.com/login");
+		//			driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+		//close current browser / or focused window
+		//			driver.close();
+		// close all browsers open by selenium / use when dealing with child windows
+		//			driver.quit();
+	}
 }
+
+
