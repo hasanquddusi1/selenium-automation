b7ef5bc43027926f2ec7a2523ba9ed4a547e783f
Muhammad Hassan Quddusi <hasan.quddusi@hotmail.com>
2021-10-04 12:42:56 -0500

    xpath customized

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,11 +1,10 @@
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 public class Demo {
 
 	// A template for a simple selenium test case
@@ -47,6 +46,18 @@ public class Demo {
 		driver.findElement(By.id("username")).sendKeys("test");
 		driver.findElement(By.id("password")).sendKeys("pass");
 		driver.findElement(By.xpath("//*[@id=\"Login\"]")).click();
+//		grab error message
+		String msgString = driver.findElement(By.cssSelector("div#error.loginError")).getText();
+		System.out.println(msgString);
+		String testString = driver.findElement(By.xpath("//a[@id='signup_link']")).getText();
+		System.out.println(testString);
+		driver.findElement(By.cssSelector("#username")).sendKeys("new user");
+		Dimension testString2 = driver.findElement(By.xpath("//img[@alt='Salesforce']")).getSize();
+		System.out.println(testString2);
+//		customized css selector from html
+		String testString3 =driver.findElement(By.cssSelector("p[class='di mr16']")).getText();
+		System.out.println(testString3);
+		
 		// close all browsers open by selenium / use when dealing with child windows
 //				driver.quit();
 	}
